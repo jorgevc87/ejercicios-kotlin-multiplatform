0667ed74d7b75c8574f0571a9dd1045947f7a795
Jorge <jorgevc_87@outlook.com>
2025-12-24 12:53:45 -0500

    expenses screen and decimal format

--- KotlinProject/composeApp/src/commonMain/kotlin/com/android/ios/kotlinproject/App.kt
+++ KotlinProject/composeApp/src/commonMain/kotlin/com/android/ios/kotlinproject/App.kt
@@ -2,8 +2,14 @@ package com.android.ios.kotlinproject
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -11,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import com.android.ios.kotlinproject.data.ExpenseManager
 import com.android.ios.kotlinproject.data.ExpenseRepoImpl
 import com.android.ios.kotlinproject.presentation.ExpensesViewModel
@@ -34,12 +42,27 @@ fun App() {
             Scaffold(
                 modifier = Modifier.fillMaxSize(),
                 topBar = {
-                TopAppBar(
-                    modifier = Modifier.background(Color.Yellow),
-                    title = { Text("This my TopBar") },
-                    navigationIcon = {},
-                    actions = {})
-            }) { innerPadding ->
+                    TopAppBar(
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+
+                                }
+                            ) {
+                                Icon(
+                                    modifier = Modifier.padding(start = 16.dp),
+                                    imageVector = Icons.Default.ArrowBack,
+                                    tint =
+                                )
+                            }
+
+                        },
+                        modifier = Modifier.fillMaxWidth().background(color = Color.Red),
+                        title = { Text("This my TopBar") },
+                        actions = {})
+                },
+                contentColor = Color.Red
+            ) { innerPadding ->
                 ExpensesScreen(Modifier.padding(innerPadding), uiState = uiState) {
 
                 }
