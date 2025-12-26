f513e35b874350499a34cdef894f827c56787c35
JorgeVC87 <jorgevc_87@outlook.com>
2025-12-25 23:48:35 -0500

    expenses app - expenses screen navigation

--- KotlinProject/composeApp/src/commonMain/kotlin/com/android/ios/kotlinproject/App.kt
+++ KotlinProject/composeApp/src/commonMain/kotlin/com/android/ios/kotlinproject/App.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Apps
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,17 +14,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.ios.kotlinproject.data.ExpenseManager
 import com.android.ios.kotlinproject.data.ExpenseRepoImpl
+import com.android.ios.kotlinproject.data.TitleTopBarTypes
 import com.android.ios.kotlinproject.presentation.ExpensesViewModel
 import com.android.ios.kotlinproject.ui.ExpensesScreen
 import moe.tlaster.precompose.PreComposeApp
 import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.path
+import moe.tlaster.precompose.navigation.rememberNavigator
 import moe.tlaster.precompose.viewmodel.viewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,41 +37,85 @@ import moe.tlaster.precompose.viewmodel.viewModel
 fun App() {
 
     PreComposeApp {
+
         val viewmodel = viewModel(modelClass = ExpensesViewModel::class) {
             ExpensesViewModel(ExpenseRepoImpl(ExpenseManager))
         }
 
         val uiState by viewmodel.uiState.collectAsStateWithLifecycle()
 
+        val colors = getColors()
+
         ExpensesTheme {
+
+            val navigator = rememberNavigator()
+            val titleTopBar = getTitleTopBar(navigator)
+
+            val isEditOrAddExpenses = titleTopBar != TitleTopBarTypes.DASHBOARD.value
+
             Scaffold(
                 modifier = Modifier.fillMaxSize(),
                 topBar = {
                     TopAppBar(
                         navigationIcon = {
-                            IconButton(
-                                onClick = {
-
+                            if (isEditOrAddExpenses) {
+                                IconButton(
+                                    onClick = {
+                                        navigator.popBackStack()
+                                    }
+                                ) {
+                                    Icon(
+                                        modifier = Modifier.padding(start = 16.dp),
+                                        imageVector = Icons.Default.ArrowBack,
+                                        tint = colors.primary,
+                                        contentDescription = "Back Arrow"
+                                    )
                                 }
-                            ) {
+                            } else {
                                 Icon(
                                     modifier = Modifier.padding(start = 16.dp),
-                                    imageVector = Icons.Default.ArrowBack,
-                                    tint =
+                                    imageVector = Icons.Default.Apps,
+                                    tint = colors.primary,
+                                    contentDescription = "Dashboard Icon"
                                 )
                             }
-
                         },
                         modifier = Modifier.fillMaxWidth().background(color = Color.Red),
-                        title = { Text("This my TopBar") },
+                        title = {
+                            Text(
+                                text = titleTopBar,
+                                fontSize = 25.sp,
+                                color = colors.primary
+                            )
+                        },
                         actions = {})
                 },
                 contentColor = Color.Red
             ) { innerPadding ->
                 ExpensesScreen(Modifier.padding(innerPadding), uiState = uiState) {
-
+                    navigator.navigate("/addExpenses/${it.id}")
                 }
             }
         }
     }
+}
+
+@Composable
+fun getTitleTopBar(navigator: Navigator): String {
+    var titleTopBar = TitleTopBarTypes.DASHBOARD
+
+    val isOnAddExpense =
+        navigator.currentEntry.collectAsState(null).value?.route?.route.equals("/addExpenses/{id}")
+    if (isOnAddExpense) {
+        titleTopBar = TitleTopBarTypes.ADD
+    }
+
+    val isEditAddExpense =
+        navigator.currentEntry.collectAsState(null).value?.path<Long>("id")
+
+    isEditAddExpense?.let {
+        titleTopBar = TitleTopBarTypes.EDIT
+    }
+
+    return titleTopBar.value
 }
---
+++ KotlinProject/composeApp/src/commonMain/kotlin/com/android/ios/kotlinproject/data/TitleTopBarTypes.kt
@@ -0,0 +1,7 @@
+package com.android.ios.kotlinproject.data
+
+enum class TitleTopBarTypes(val value: String) {
+    DASHBOARD("DashBoard"),
+    ADD("Add Expense"),
+    EDIT("Edit Expense")
+}
